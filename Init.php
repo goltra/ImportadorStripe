11fceaec9788764121df2c62ef23249a8336f01a
Francisco Garcia <franciscogarcia@Franciscos-MacBook-Air.local>
2021-12-12 09:35:56 +0100

    instalo la librería de stripe con composer

--- Init.php
+++ Init.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace FacturaScripts\Plugins\ImportadorStripe;
-
+require_once __DIR__ . '/vendor/autoload.php';
 class Init extends \FacturaScripts\Core\Base\InitClass
 {
     public function init()
     {
         /// se ejecutara cada vez que carga FacturaScripts (si este plugin está activado).
-        require_once('stripe/init.php');
+
     }
 
     public function update()

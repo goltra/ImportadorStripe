0c3542ca8819154586b192d5f1bf7a2ba688696d
Jose <jose@goltratec.com>
2025-08-20 17:08:38 +0200

    Agrego parámetros para crear la remesa y envio email al terminar

--- Controller/TestTransaction.php
+++ Controller/TestTransaction.php
@@ -9,10 +9,17 @@ namespace FacturaScripts\Plugins\ImportadorStripe\Controller;
 
 use FacturaScripts\Core\Base\Controller;
 use FacturaScripts\Core\Base\DataBase\DataBaseWhere;
+use FacturaScripts\Dinamic\Lib\Email\NewMail;
 use FacturaScripts\Dinamic\Model\ReciboCliente;
 use FacturaScripts\Dinamic\Model\RemesaSEPA;
-use FacturaScripts\Plugins\ImportadorStripe\Model\BalanceTransactionModel;
-use FacturaScripts\Plugins\ImportadorStripe\Model\PayoutModel;
+use FacturaScripts\Plugins\ImportadorStripe\Model\SettingStripeModel;
+use PHPMailer\PHPMailer\Exception;
+use Stripe\Exception\ApiErrorException;
+use Stripe\Invoice;
+use Stripe\StripeClient;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 
 class TestTransaction extends Controller
@@ -36,25 +43,25 @@ class TestTransaction extends Controller
 
     public function init(){
 
-//        Documentación de lo que hay que hacer aquí: https://github.com/goltra/ImportadorStripe/issues/3
-
-        echo '<pre>';
-        $payoutId = 'po_1QhK6gHDuQaJAlOmouHWIs8M';
+//        $payoutId = 'po_1QhK6gHDuQaJAlOmouHWIs8M';
+        $payoutId = 'po_1R1clUHDuQaJAlOmPOZRnWtO';
         $sk = 'sk_test_51ILOeaHDuQaJAlOmoxCwXO9mYqMKmXk6c9ByTDILdJ3vujXorxScbbyTNBrQeXb82oNeqq4UsioajKWiSaRMEGL700xoDW92tk';
 
-        $stripe = new \Stripe\StripeClient($sk);
+        $stripe = new StripeClient($sk);
 
         //  Pido los datos del pago
         $payout = $stripe->payouts->retrieve($payoutId, []);
 
         //  Creo la remesa
         $remesa = new RemesaSEPA();
+
         //  TODO: revisar este nombre donde aparece y que quieren que se ponga aquí.
-        $remesa->nombre = 'Pago Enero CJL';
+        $remesa->nombre = 'Pago Stripe Enero CJL';
         $remesa->descripcion = 'Pago Enero CJL';
         $remesa->fecha = date('Y-m-d H:i:s');
         $remesa->fechacargo  = date('Y-m-d', $payout['arrival_date']);
-        // todo, el código de cuenta tiene que ir automático.
+
+        // TODO: el código de cuenta tiene que ir automático.
         $remesa->codcuenta = 1;
         $remesa->save();
 
@@ -67,66 +74,117 @@ class TestTransaction extends Controller
         $total = 0;
         $errors = [];
 
+
         foreach ($balanceTransaction->data as $transaction) {
 
-            if (empty($transaction['source']))
+            if (empty($transaction['source'])){
                 continue;
-
-            //  es un cargo
-            if (strpos($transaction['source'], 'ch_') === 0) {
-                $charge = $stripe->charges->retrieve($transaction['source'], []);
-
-                if (!empty($charge->invoice)) {
-                    $invoice = $stripe->invoices->retrieve($charge->invoice, []);
-                    $total += $invoice['amount_paid'] / 100;
-
-                    $facturaId = $invoice->metadata['fs_idFactura'];
-
-                    if (!isset($facturaId)){
-                        $errors[$invoice['id']] = 'La factura no está vinculada en stripe.';
-                        continue;
-                    }
-
-                    $reciboCliente = new ReciboCliente();
-
-                    $reciboCliente->loadFromCode('', [new DataBaseWhere('idfactura', $facturaId), new DataBaseWhere('pagado', false)]);
-
-                    if (!$reciboCliente->idrecibo){
-                        $errors[$invoice['id']] = 'La factura asignada no tiene recibos';
-                        continue;
-                    }
-
-
-                    if ($reciboCliente->idremesa){
-                        $errors[$invoice['id']] = 'La factura ya tiene una remesa asignada';
-                        continue;
-                    }
-
-                    $reciboCliente->idremesa = $remesa->idremesa;
-                    $reciboCliente->save();
-
-
-                    var_dump($invoice['id'] . ' - ' .$facturaId. ' - ' . $invoice['amount_paid'] / 100);
-                }
             }
 
-            //  es un pago
-//            if (strpos($transaction['source'], 'in_') === 0) {
-//                var_dump($transaction['id']);
-//
-//                $invoice = $stripe->invoices->retrieve($transaction['source'], []);
-//
-//                var_dump($invoice['id'] . ' - ' . $invoice['amount_paid'] / 100);
-//                $total += $invoice['amount_paid'] / 100;
-//            }
+            if (!($invoice = $this->getInvoiceFromTransaction($stripe, $transaction['source'], $errors))) {
+                $errors[] = 'No se puede obtener la factura de stripe. Source: ' . $transaction['source'];
+                continue;
+            }
+
+
+            $total += $invoice['amount_paid'] / 100;
+
+            $facturaId = $invoice->metadata['fs_idFactura'];
+
+            if (!isset($facturaId)){
+                $errors[$invoice['id']] = 'La factura no está vinculada en stripe.';
+                continue;
+            }
+
+            $reciboCliente = new ReciboCliente();
+
+            $where = [
+                new DataBaseWhere('idfactura', $facturaId),
+                new DataBaseWhere('pagado', false),
+            ];
+
+            $reciboCliente->loadFromData($where);
+
+            if (!$reciboCliente->idrecibo){
+                $errors[$invoice['id']] = 'La factura asignada no tiene recibos';
+                continue;
+            }
+
+            if ($reciboCliente->idremesa){
+                $errors[$invoice['id']] = 'La factura ya tiene una remesa asignada';
+                continue;
+            }
+
+            $reciboCliente->idremesa = $remesa->idremesa;
+            $reciboCliente->pagado = true;
+            $reciboCliente->save();
+
+            var_dump($invoice['id'] . ' - ' . $facturaId . ' - ' . $invoice['amount_paid'] / 100);
         }
 
         var_dump($errors);
         var_dump('total transferencia: '. $total);
 
 
+        if (SettingStripeModel::getSetting('adminEmail'))
+            $this->sendMail($errors, $total);
 
-        //
 
+    }
+
+    /**
+     *  En un pago pueden venir varias procedencias de cobro.
+     *  ch >> Es mediante cargo, que es la que nos interesa
+     *  in >> Es una factura
+     * @param StripeClient $stripe
+     * @param string $source
+     * @param array $errors
+     * @return Invoice|null
+     * @throws ApiErrorException
+     */
+    private function getInvoiceFromTransaction(StripeClient $stripe, string $source, array &$errors): ?Invoice
+    {
+        // Caso: cargo (charge)
+        if (str_starts_with($source, 'ch_')) {
+            $charge = $stripe->charges->retrieve($source, []);
+
+            if (empty($charge->invoice)) {
+                $errors[] = 'El cargo ' . $source . ' no tiene factura';
+                return null;
+            }
+
+            return $stripe->invoices->retrieve($charge->invoice, []);
+        }
+
+        // Caso: factura directa (invoice)
+        if (str_starts_with($source, 'in_')) {
+            return $stripe->invoices->retrieve($source, []);
+        }
+
+        return null;
+    }
+
+    /**
+     * Método que va a mandar un email
+     * @param $errors
+     * @param $total
+     * @return void
+     * @throws Exception
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    private function sendMail($errors, $total): void
+    {
+        $subject = 'Nueva remesa de cobro de stripe creada';
+        $body = "Total transferencia: " . $total . "\n\n";
+        $body .= "Errores:\n" . implode("\n", $errors);
+
+        $mail = NewMail::create()
+            ->to(SettingStripeModel::getSetting('adminEmail'))
+            ->subject($subject)
+            ->body(nl2br($body));
+
+        $mail->send();
     }
 }
